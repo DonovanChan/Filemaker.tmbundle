4709ddf52848a5288a702bb060549cede927a5a7
Donovan Chandler <donovan_c@beezwax.net>
2012-06-06 21:49:20 -0700

    Enhanced error handling and passing of string to shell script.

--- Support/lib/filemaker/clipboard.rb
+++ Support/lib/filemaker/clipboard.rb
@@ -86,9 +86,16 @@ module FileMaker::Clipboard
   # @return [String,nil] XML that was loaded to the clipboard. Returns nil in case of error.
   def self.set(object)
     text = self.escape_for_shell(object.to_xml)
-    shellScript = %Q[osascript "#{PATH_PASTE}" '#{text}']
+    # Use xargs -0 so we can provide long strings with single quotes and spaces
+    # osascript '-s o' option prints errors to stdout
+    shellScript = %Q[echo '#{text}'|xargs -0 osascript -s o "#{PATH_PASTE}"]
     result = `#{shellScript}`
-    raise ArgumentError, "Invalid/Unsupported string. Could be trouble with extended ascii character.\n#{result}" if result.start_with?('Error')
+    result.gsub!(/\r/,"\n") if result
+    if result.start_with?('Error validating XML')
+      raise ArgumentError, result
+    elsif result.start_with?('Error')
+      raise ArgumentError, "Invalid/Unsupported string. Could be trouble with extended ascii character.\n#{result}"
+    end
     return result
   end
   
